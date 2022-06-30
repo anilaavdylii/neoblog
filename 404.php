ef40e8b91290b78553faa74bedde0557b7938d36
anilaavdylii <aa48857@ubt-uni.net>
2022-07-01 01:12:56 +0200

    added 404 page and footer section

--- 404.php
+++ 404.php
@@ -1,10 +1,61 @@
-<?php get_header();?>
+<?php 
+// get header-color.php 
+get_header("color");
+?>
 
-<section class="error">
-    <h4>Page not Found...!</h4>
-</section>
+      <!-- categories-section -->
+      <section class="categories">
+        <div class="container">
+          <div class="flex flex-row flex-wrap">
+            <?php
+            // get template_part tag
+            get_template_part("templates/temp/categories","content")
+            ?>
+          </div>
+        </div>
+      </section>
+      <!-- End-categories -->
 
+      <!-- Post-are -->
+      <section class="post-area">
+        <div class="wrapper">
+          <!-- two-columns-layout -->
+          <div class="row">
+            <div class="col-8-lg col-12-md">
+             <?php echo wpautop("Sorry page Not found"); ?>
 
+              <!-- pagination area -->
+              <div class="row">
+                <div class="col-12-lg text-center">
+                  <div class="pagination">
+                    <?php
+                    echo paginate_links(array(
+                      'mid-size'=> 3,
+                      'prev_text'=> __('<span class="fa fa-arrow-left"></span>'),
+                      'next_text'=> __('<span class="fa fa-arrow-right"></span>')
+                    ));
+                    ?>
+                    <!-- <a href="#"><span>1</span></a>
+                    <a href="#"><span>2</span></a>
+                    <a href="#"><span>3</span></a>
+                    <a href="#"><span class="fa fa-arrow-right"></span></a> -->
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-4-lg col-12-md">
+              <?php
+              // get sidebar-template
+              get_sidebar();
+              ?>
+            </div>
+          </div>
+        </div>
+      </section>
+      <!-- End-post-area -->
 
-
-<?php get_footer();?>
+      <?php
+      // get footer.php 
+      get_footer();
+      ?>
+    
--- footer.php
+++ footer.php
@@ -1,17 +1,87 @@
 </main>
 
-<?php 
-        if(has_nav_menu("footer-menu")){
-            wp_nav_menu(array("theme_location"=> 'footer-menu'));
-        }
-    ?>
+    <!-- End-main site -->
 
-<footer>
-    <p>&copy; Copyrights <a href="<?php bloginfo('url')?>"><?php bloginfo('name')?></a>. All Rights Reserved</p>
-</footer>
+    <!-- footer area -->
+    <footer class="footer-area">
+      <div class="row">
+        <div class="col-4-lg col-4-md col-12-sm">
+          <!-- about me -->
+          <section class="about-me">
+            <div class="site-logo">
+              <!-- <img
+                src="./assets/images/logo.png"
+                width="60"
+                alt="footer logo"
+              /> -->
+              <?php
+           if (function_exists("the_custom_logo")) {
+            the_custom_logo();
+           }
+           else {
+            bloginfo("name");
+           }
+         
+           ?>
+            </div>
+            <p class="para">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores,
+              expedita! Dicta ipsa debitis quia rerum obcaecati autem facere,
+              eaque consectetur.
+            </p>
+            <div class="social-media">
+              <a href="#" class="text-primary"
+                ><span class="fab fa-facebook"></span
+              ></a>
+              <a href="#" class="text-primary"
+                ><span class="fab fa-instagram"></span
+              ></a>
+              <a href="#" class="text-primary"
+                ><span class="fab fa-twitter"></span
+              ></a>
+              <a href="#" class="text-primary"
+                ><span class="fab fa-youtube"></span
+              ></a>
+            </div>
+          </section>
+        </div>
+        <div class="col-4-lg col-4-md col-12-sm"></div>
+        <!-- recent post-area -->
+        <section class="recent-post">
+          <a href="#" class="text-md text-light">Recent Post</a>
+          <div class="posts flex flex-row flex-wrap">
+           <?php 
+        //    get template file recent-post.php
+        get_template_part("templates/temp/recent", "post");
+           ?>
+          </div>
+        </section>
+      </div>
+      <div class="col-4-lg col-4-md col-12-sm">
+        <!-- popular tags -->
+        <section class="popular-tags">
+          <a href="#" class="text-md text-light">Popular Tags</a>
+          <div class="tags flex flex-row flex-wrap">
+              <?php
+            //   get template file  post-tags.php 
+            get_template_part("templates/temp/post", "tags");
+               ?>
+            <!-- <a href="#"><span>Camera</span></a>
+            <a href="#"><span>photography</span></a>
+            <a href="#"><span>Blog</span></a>
+            <a href="#"><span>Tech</span></a>
+            <a href="#"><span>Travel</span></a> -->
+          </div>
+        </section>
+      </div>
+      <!-- </div> -->
+      <div class="rights text-center text-gray">
+        Wordpress Themes by
+        <a href="<?php echo home_url("/"); ?>" class="text-primary">Ali Dhuniya</a> &copy; <?php the_date("Y"); ?>
+      </div>
+    </footer>
+    <!-- End-footer area -->
 
-<?php dynamic_sidebar('footer_sidebar');?>
-<?php wp_footer()?>
-</body>
-
+    
+  </body>
 </html>
