6f68d317d02b3bb4b08369aec3d7d8d60f3da4b4
anilaavdylii <aa48857@ubt-uni.net>
2022-07-02 20:21:28 +0200

    added about page

---
+++ about.php
@@ -0,0 +1,54 @@
+
+<?php
+/**
+ * Template Name: About Page
+ * The template for displaying contact form, only an example template.
+ * No stylesheet or javascript includes on this example
+ *
+ */
+
+ get_header("color");?>
+
+<header class='masthead'>
+  <p class='masthead-intro'>HELLO THERE!</p>
+  <h1 class='masthead-heading'>Welcome to my blog!</h1>
+</header>
+
+<section class="introduction-section">
+    <h1>Introduction</h1>
+    <p>An entrepreneur, writer, philanthropist & unshakable optimist dedicated to helping you become the person you most want to be.</p>
+    <p>A born-and-raised Jersey girl with nothing more than passion, a laptop and a dream, I’m proud to have created a socially conscious digital empire that touches millions.
+
+Through our award-winning show MarieTV, world-class online training programs, a #1 New York Times bestselling book and an audience in 195 countries, I help people like you dream big and back it up with meaningful action to create results..</p>
+</section>
+<section class="location-section">
+    <h1>Where I'm From</h1>
+    <p>I'm originally from Toronto, Ontario. </p>
+</section>
+
+<section class="image-gallery">
+<div class="box">
+    <img src="https://i.pinimg.com/564x/5e/df/dd/5edfdd323ccd316b6e31a746d3d12355.jpg" alt="first">
+    <img src="https://i.pinimg.com/564x/9b/be/09/9bbe096331d1b7df49091e2e6a25ce29.jpg" alt="second">
+  </div>
+</div>
+
+
+</section>
+
+<br><br><br><br><br><br><br>
+<section class="questions-section">
+    <h1>More About Me</h1>
+    <h2>What are your favorite hobbies?</h2>
+    <p>My favorite hobby is building things on the internet like ecommerce sites and email marketing campaigns.</p>
+    <h2>Where do you live?</h2>
+    <p>I live on a rural acreage, but I'm close to Ottawa and Montreal.</p>
+    <h2>Why are you a blogger?</h2>
+    <p>Because blogging gives you A platform for your voice to be heard.</p>
+</section>
+<br><br><br>
+
+      <?php
+      get_footer();
+      ?>
+    
