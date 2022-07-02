a9c10f3c270b1da1dd7cbea0a668c07aa546c3dc
anilaavdylii <aa48857@ubt-uni.net>
2022-07-02 19:19:35 +0200

    added contact page

---
+++ contact-page.php
@@ -0,0 +1,61 @@
+
+<?php
+/**
+ * Template Name: Contact Page
+ * The template for displaying contact form, only an example template.
+ * No stylesheet or javascript includes on this example
+ *
+ */
+
+get_header("color");
+?>
+
+
+      <section class="post-area">
+        <div class="wrapper">
+          <div class="row">
+            <div class="col-lg-12">
+            <?php if(have_posts()): ?>
+<?php while(have_posts()): the_post();?>
+
+<div class="article">
+
+                  <article class="single-post">
+                  <div class="post-title">
+                        <a href="<?php the_permalink(); ?>"
+                          ><span class="text-lg text-dark"
+                            ><?php echo the_title(); ?></span
+                          >
+                        </a>
+                      </div>
+                    <div class="post-thumbnail">
+                      <?php
+                      if (has_post_thumbnail()):?>
+                      <img
+                        src="<?php echo get_the_post_thumbnail_url(); ?>"
+                        alt="article1"
+                        class="fluid"
+                      />
+                      <?php endif; ?>
+                    </div>
+                    <div class="post-content">
+                        <p class="para">
+                         <?php echo get_the_content(); ?>
+                        </p>
+                      </div>
+                  </article>
+                </div>
+
+<?php endwhile; ?>
+<?php else: ?>
+    <?php echo wpautop("Sorry No Post were Found");?>
+<?php endif; ?>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <?php
+      get_footer();
+      ?>
+    
