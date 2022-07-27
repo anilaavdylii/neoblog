a695ce782a8e51f2e690f4ce45cdcc647e875646
anilaavdylii <aa48857@ubt-uni.net>
2022-07-27 13:55:20 +0200

    added dynamic content in about page

--- about.php
+++ about.php
@@ -1,4 +1,3 @@
-
 <?php
 /**
  * Template Name: About Page
@@ -8,43 +7,15 @@
  get_header("color");?>
 
 <header class='masthead'>
-  <p class='masthead-intro'>HELLO THERE!</p>
+  <p class='masthead-intro'><?php the_title(  ); ?></p>
 </header>
 
 <section class="introduction-section">
-    <h1>Introduction</h1>
-    <p>An entrepreneur, writer, philanthropist & unshakable optimist dedicated to helping you become the person you most want to be.</p>
-    <p>A born-and-raised Jersey girl with nothing more than passion, a laptop and a dream, I’m proud to have created a socially conscious digital empire that touches millions.
-
-Through our award-winning show MarieTV, world-class online training programs, a #1 New York Times bestselling book and an audience in 195 countries, I help people like you dream big and back it up with meaningful action to create results..</p>
-</section>
-<section class="location-section">
-    <h1>Where I'm From</h1>
-    <p>I'm originally from Toronto, Ontario. </p>
+  <?php echo get_the_post_thumbnail_url(); ?>
+  <?php the_content( '<p>', '</p>' ); ?>
 </section>
 
-<section class="image-gallery">
-<div class="box">
-    <img src="https://i.pinimg.com/564x/5e/df/dd/5edfdd323ccd316b6e31a746d3d12355.jpg" alt="first">
-    <img src="https://i.pinimg.com/564x/9b/be/09/9bbe096331d1b7df49091e2e6a25ce29.jpg" alt="second">
-  </div>
-</div>
-
-
-</section>
-
-<br><br><br><br><br><br><br>
-<section class="questions-section">
-    <h1>More About Me</h1>
-    <h2>What are your favorite hobbies?</h2>
-    <p>My favorite hobby is building things on the internet like ecommerce sites and email marketing campaigns.</p>
-    <h2>Where do you live?</h2>
-    <p>I live on a rural acreage, but I'm close to Ottawa and Montreal.</p>
-    <h2>Why are you a blogger?</h2>
-    <p>Because blogging gives you A platform for your voice to be heard.</p>
-</section>
-<br><br><br>
-
+<br><br>
       <?php
       get_footer();
       ?>
