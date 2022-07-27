ef18ac7acbb5a68671261e4143306adc769458dd
anilaavdylii <aa48857@ubt-uni.net>
2022-07-27 14:45:04 +0200

    updated 404 page

--- 404.php
+++ 404.php
@@ -1,42 +1,17 @@
-<?php 
-get_header("color");
-?>
+<?php
+/**
+ * Template Name: 404 Page
+ *
+ */
 
-      <section class="categories">
-        <div class="container">
-          <div class="flex flex-row flex-wrap">
-            <?php
-            get_template_part("templates/temp/categories","content")
-            ?>
-          </div>
-        </div>
-      </section>
-
+ get_header("color");?>
+ 
       <section class="post-area">
         <div class="wrapper">
-          <div class="row">
-            <div class="col-8-lg col-12-md">
-             <?php echo wpautop("Sorry page Not found"); ?>
-
-              <div class="row">
-                <div class="col-12-lg text-center">
-                  <div class="pagination">
-                    <?php
-                    echo paginate_links(array(
-                      'mid-size'=> 3,
-                      'prev_text'=> __('<span class="fa fa-arrow-left"></span>'),
-                      'next_text'=> __('<span class="fa fa-arrow-right"></span>')
-                    ));
-                    ?>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="col-4-lg col-12-md">
-              <?php
-              get_sidebar();
-              ?>
-            </div>
+            <div class="col-12-lg col-12-md" style="text-align: center">
+            <br><br>
+            <img src="https://i.pinimg.com/originals/a4/62/d1/a462d192479048db0f02f4466b900e0a.gif" height="400px" width="600px">
+             <?php echo wpautop("Sorry, page not found"); ?>
           </div>
         </div>
       </section>
